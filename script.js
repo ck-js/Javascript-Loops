cfd09d897686d4e4b18a47b89b0f8e6a16821f59
Cliffton Kaewkarn <ck@Clifftons-MacBook-Air.local>
2023-03-10 20:52:15 +0700

    Add code examples of self assigning variables, increment and decrement operators and while loops with conditions that equal true fron variables outside of the code block

---
+++ script.js
@@ -0,0 +1,40 @@
+
+let wallet = 3;
+wallet = wallet + 2;
+wallet = wallet -1;
+console.log(wallet);
+
+let nameInfo = "Account Name:";
+nameInfo = nameInfo + " Elon";
+nameInfo = nameInfo + " Musk";
+console.log(nameInfo);
+
+let likes = 5;
+likes += 5;
+console.log(likes);
+
+let drinks = 10;
+drinks -= 1;
+console.log(drinks);
+
+let pets = 1;
+pets ++;
+console.log(pets);
+
+ pets = 11;
+pets --;
+console.log(pets);
+
+let keepGoing = true;
+while (keepGoing === true) {
+    console.log("Keep going: " + keepGoing);
+keepGoing = false;
+}
+console.log("Keep going: " + keepGoing);
+
+let autopilotOn = false;
+while (autopilotOn === true) {
+    console.log("Autopilot is now activated");
+    autopilotOn = false;
+}
+
